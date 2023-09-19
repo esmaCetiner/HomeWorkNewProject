f2f55ea2b8ecbb8cbbc80138939f57df1380b6b0
esmaCetiner <esma.cetiner2016@gmail.com>
2023-09-19 15:06:03 +0200

    ADD LOCALLY!

--- src/NewProject/HomeWorkTest.java
+++ src/NewProject/HomeWorkTest.java
@@ -3,5 +3,6 @@ package NewProject;
 public class HomeWorkTest {
     public static void main(String[] args) {
         System.out.println("Odev dosyasi olusturuldu.");
+        System.out.println("ADD LOCALLY!");
     }
 }
