d4b236487ba1c030d219adc8a873942e48a61f6d
esmaCetiner <esma.cetiner2016@gmail.com>
2023-09-19 15:14:35 +0200

    ekleme yapildi

--- src/NewProject/HomeWorkTest.java
+++ src/NewProject/HomeWorkTest.java
@@ -4,5 +4,6 @@ public class HomeWorkTest {
     public static void main(String[] args) {
         System.out.println("Odev dosyasi olusturuldu.");
         System.out.println("ADD LOCALLY!");
+        System.out.println("ADDED LOCALLY, pushed with git menu.");
     }
 }
--- src/NewProject/HomeWorkTest2.java
+++ src/NewProject/HomeWorkTest2.java
@@ -1,4 +1,7 @@
 package NewProject;
 
 public class HomeWorkTest2 {
+    public static void main(String[] args) {
+
+    }
 }
